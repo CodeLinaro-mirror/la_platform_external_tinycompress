736525a8412fdaf3fbad3bd2570e82d916678d9c
Fei Tong <quic_ftong@quicinc.com>
2019-11-04 15:06:23 -0800

    tinycompress: enable EXTENDED_COMPRESS_FORMAT based on flag
    Enable EXTENDED_COMPRESS_FORMAT based on flag
    
    CRs-Fixed: 2493949
    Change-Id: Ic12b0e2610b6f55c8d8a16f646daa016733f0afb

--- Android.mk
+++ Android.mk
@@ -2,11 +2,14 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CFLAGS := -Wno-macro-redefined -Wno-unused-function
+ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)), true)
+LOCAL_CFLAGS += -DENABLE_EXTENDED_COMPRESS_FORMAT
+endif
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-LOCAL_SRC_FILES := compress.c utils.c
+LOCAL_SRC_FILES := compress.c utils.c compress_hw.c compress_plugin.c snd_utils.c
 LOCAL_MODULE := libtinycompress
 LOCAL_SHARED_LIBRARIES := libcutils libutils
 LOCAL_MODULE_TAGS := optional
@@ -32,7 +35,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-LOCAL_SRC_FILES := compress.c utils.c
+LOCAL_SRC_FILES := compress.c utils.c compress_hw.c compress_plugin.c snd_utils.c
 LOCAL_CFLAGS += -DUSE_VENDOR_EXTN
 LOCAL_MODULE := libtinycompress_vendor
 LOCAL_SHARED_LIBRARIES := libcutils libutils
